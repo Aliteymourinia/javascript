6a7a81ccc23a7fc2902b14b407bff96f5a0ff47f
Aliteymourinia <aliteywb@gmail.com>
2023-10-09 22:51:53 +0330

    practice

---
+++ logic/app.js
@@ -0,0 +1,21 @@
+// function sayInfo(name, age) {
+//   const msg = "your name is " + name + " and age is " + age;
+//   alert(msg);
+// }
+// sayInfo("Ali", 22);
+
+// const sayName = (name, age) => {
+//   alert("your name is " + name + " your age is" + age);
+// };
+// sayName("navid", 10);
+// const sayNameWith = (name, age) =>
+//   alert(`your name is ${name} your age is ${age}`);
+// sayNameWith("navid inter", 10);
+
+// const return2 = () => 2;
+// const wrongReturn = () => {
+//   return 2;
+// };
+// console.log(return2(), wrongReturn());
+const birtday=1995;
+alert("birthday year is 1995")
