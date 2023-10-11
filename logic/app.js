6b7c1945a8109e2de26153e04c9f9ff6be28f573
Aliteymourinia <aliteywb@gmail.com>
2023-10-11 20:24:51 +0330

    add tailwind with package

--- logic/app.js
+++ logic/app.js
@@ -1,21 +1,8 @@
-// function sayInfo(name, age) {
-//   const msg = "your name is " + name + " and age is " + age;
-//   alert(msg);
-// }
-// sayInfo("Ali", 22);
-
-// const sayName = (name, age) => {
-//   alert("your name is " + name + " your age is" + age);
-// };
-// sayName("navid", 10);
-// const sayNameWith = (name, age) =>
-//   alert(`your name is ${name} your age is ${age}`);
-// sayNameWith("navid inter", 10);
-
-// const return2 = () => 2;
-// const wrongReturn = () => {
-//   return 2;
-// };
-// console.log(return2(), wrongReturn());
-const birtday=1995;
-alert("birthday year is 1995")
+const food = ["rice", "chick", "fish"];
+food.push("pizza");
+// console.log(food[2]);
+// console.log(food.length);
+const Foods = [["bread", "soup"], [433]];
+Foods.push(["dude"]);
+console.log(Foods[0][1]);
+document.querySelector("frm")
