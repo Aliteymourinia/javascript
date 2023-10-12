7584de0f920fcd3298c32cccda55be84b4deca32
Aliteymourinia <aliteywb@gmail.com>
2023-10-12 14:03:31 +0330

    add quote generator

--- logic/app.js
+++ logic/app.js
@@ -1,8 +1,29 @@
-const food = ["rice", "chick", "fish"];
-food.push("pizza");
-// console.log(food[2]);
-// console.log(food.length);
-const Foods = [["bread", "soup"], [433]];
-Foods.push(["dude"]);
-console.log(Foods[0][1]);
-document.querySelector("frm")
+const quote = document.querySelector("#quote");
+const btn = document.querySelector("#getQoute");
+const writer = document.querySelector("#writer");
+const loading = document.querySelector("#loading");
+
+fetch("https://api.themotivate365.com/stoic-quote")
+  .then((res) => res.json())
+  .then((data) => {
+    localStorage.setItem("quote" + crypto.randomUUID(), JSON.stringify(data));
+
+    writer.textContent = data.author;
+    quote.textContent = data.quote;
+  });
+
+btn.addEventListener("click", () => {
+  loading.classList.remove("hidden");
+
+  fetch("https://api.themotivate365.com/stoic-quote")
+    .then((res) => res.json())
+    .then((data) => {
+      localStorage.setItem("quote" + crypto.randomUUID(), JSON.stringify(data));
+
+      writer.textContent = data.author;
+      quote.textContent = data.quote;
+    })
+    .finally(() => {
+      loading.classList.add("hidden");
+    });
+});
