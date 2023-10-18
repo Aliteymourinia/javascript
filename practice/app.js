14e6e6db16432ac49f4d4722dd72560cdabe4919
Aliteymourinia <aliteywb@gmail.com>
2023-10-18 19:57:16 +0330

    practice some codes

--- practice/app.js
+++ practice/app.js
@@ -58,7 +58,7 @@ info("ali", " tey"); //this is argument
 const inform = (bye) => {
   console.log(bye);
 };
-inform("bye");
+inform("goodbye");
 
 // math = (x, y) => {
 //   return x * y;
@@ -66,9 +66,96 @@ inform("bye");
 // let result = math(50, 2);
 // console.log(result);
 
-
 function math(x, y) {
   return x + y;
 }
-let result = math(2, 4);
-console.log(result);
+let resultt = math(2, 4);
+console.log(resultt);
+
+//array object:
+
+const courses = [
+  {
+    id: 1,
+    name: "html",
+    time: 10,
+    isCompleted: true,
+  },
+  {
+    id: 2,
+    name: "javascript",
+    time: "null",
+    isCompleted: false,
+  },
+];
+console.log(courses);
+console.log(courses[0]);
+
+// console.log(courses[0])
+// const result=JSON.stringify(courses);
+// console.log(result) // this is for convert to JSON.
+
+//loop:
+for (let i = 0; i < 10; ) {
+  i += 2;
+  console.log(i);
+}
+//while => this is for loop
+
+//high order array methods
+//forEach , map , filter
+
+//conditions:
+//(if, else, else if, &&, ||)
+
+const bbs = [(a = 5), (b = 5)];
+if (a === b) {
+  console.log("برابر");
+}
+
+const A = 8;
+const B = 6;
+
+if (A === B) {
+  console.log("equal");
+} else if (A < B) {
+  console.log("not equal");
+} else {
+  console.log("true");
+}
+//ternary operator: meghdar dehi vriable bar asas shart.
+const x = 8;
+const color = x > 10 ? "red" : "blue";
+console.log(color);
+
+//switch:
+const color2 = "red";
+switch (color2) {
+  case "red":
+    console.log("its red");
+    break;
+  case "blue":
+    console.log("its blue");
+    break;
+  default:
+    console.log("color is not red or blue");
+}
+const car = "ford";
+switch (car) {
+  case "bmw":
+    console.log("true");
+    break;
+  case "benz":
+    console.log("false");
+    break;
+  default:
+    console.log("this car is not bmw or benz");
+}
+//arrow function:
+const addNum = (num1 = 5, num2 = 10) => {
+  console.log(num1 + num2);
+};
+addNum(5, 10);
+
+//fat arrow =>
+
