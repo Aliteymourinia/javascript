56d92851a45e000d6d6e77227b658aa1787c6c21
Aliteymourinia <aliteywb@gmail.com>
2023-10-31 20:29:02 +0330

    create early stages of expanding cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    backgroundSize: {
+      auto: "auto",
+      "100%": "100%",
+    },
+    plugins: [],
   },
-  plugins: [],
 };
