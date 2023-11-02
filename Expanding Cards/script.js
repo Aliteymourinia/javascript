6f7378a88c01d638a79d9c5fc2d83d23a8828962
Aliteymourinia <aliteywb@gmail.com>
2023-11-02 21:39:46 +0330

    finish up js

---
+++ Expanding Cards/script.js
@@ -0,0 +1,23 @@
+// const panels = document.querySelectorAll(".panel");
+// panels.forEach((panel) => {
+//   panel.addEventListener("click", () => {
+//     panels.forEach((otherPanel) => {
+//       otherPanel.classList.remove("grow");
+//     });
+//     panel.classList.add("grow");
+//   });
+// });
+const container = document.querySelector(".container");
+const panels = container.querySelectorAll(".panel");
+
+container.addEventListener("click", (e) => {
+  const clickedPanel = e.target.closest(".panel");
+  if (clickedPanel)
+    panels.forEach((panel) => {
+      const h3 = panel.querySelector("h3");
+      h3.classList.add("opacity-0");
+      panel.classList.remove("grow");
+    });
+  clickedPanel.classList.add("grow");
+  clickedPanel.querySelector("h3").classList.remove("opacity-0");
+});
