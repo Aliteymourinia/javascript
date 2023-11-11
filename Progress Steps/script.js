4e13646548cd18e9ffd6797695c803f46ef5c8ab
Aliteymourinia <aliteywb@gmail.com>
2023-11-11 18:29:50 +0330

    add js codes

---
+++ Progress Steps/script.js
@@ -0,0 +1,12 @@
+const progress = document.querySelector(".progress");
+const btnPrimary = document.querySelector(".btnPrimary");
+btnPrimary.addEventListener("click", () => {
+  const clickedButton = target(".btnPrimary");
+  if (clickedButton)
+ progress.document.querySelector(".progress")
+progress.classList.add("w-24")
+});
+
+btnPrimary.classList.add("");
+
+console.log(btn);
