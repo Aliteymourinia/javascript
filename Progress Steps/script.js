4753669ff1a4f37b5475d61e7aa68f805845a807
Aliteymourinia <aliteywb@gmail.com>
2023-11-14 16:57:40 +0330

    make interactive progress

--- Progress Steps/script.js
+++ Progress Steps/script.js
@@ -5,5 +5,25 @@ const circles = document.querySelectorAll(".circle");
 let currentActive = 1;
 next.addEventListener("click", () => {
   currentActive++;
-  console.log(currentActive);
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
 });
+
+// prev.addEventListener("Click", () => {
+//   currentActive--;
+//   if (currentActive < 1) {
+//     currentActive = 1;
+//   }
+//   update();
+// });
+// function update() {
+//   circles.forEach((circle, idx) => {
+//     if (idx < currentActive) {
+//       circle.classList.add("border-blue-500");
+//     } else {
+//       circle.classList.remove("border-blue-500");
+//     }
+//   });
+// }
