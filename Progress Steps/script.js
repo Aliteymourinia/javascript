3cb02268db9d8aaf12119e747316be5d1b88ccd3
Aliteymourinia <aliteywb@gmail.com>
2023-11-24 22:48:38 +0330

    make interactive progress with add style to js

--- Progress Steps/script.js
+++ Progress Steps/script.js
@@ -1,10 +1,25 @@
 const progress = document.querySelector("#progress");
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
-const circle = document.querySelectorAll(".circle");
-let currentActive=1;
-next.addEventListener("click", () => {   
-  currentActive++
+const circles = document.querySelectorAll(".circle");
+let currentActive = 1;
+const pixelValues = [, 380, 600, 990];
+next.addEventListener("click", () => {
+  currentActive++;
+  progress.style.width = pixelValues[currentActive - 1] + "px";
+  circles[currentActive - 1].style.borderColor = "#3B82F6";
+  prev.style.backgroundColor = "#3B82F6";
+  prev.style.borderColor = "#3B83F6";
+  prev.disabled = false;
+  prev.addEventListener("click", () => {
+    currentActive--;
+    if (currentActive >= 1) {
+      progress.style.width = pixelValues[currentActive - 1] + "px";
+    }
+  });
+});
 
-  })
-  console.log(currentActive)
+console.log(currentActive);
+
+// currentActive--;
+// progress.style.width = pixelValues[currentActive-1] + "px";
