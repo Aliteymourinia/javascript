cb9b2a1df71bc9859646b64d3af017ff2faa2b87
Aliteymourinia <aliteywb@gmail.com>
2023-11-24 20:30:08 +0330

    add  some changes

--- Progress Steps/script.js
+++ Progress Steps/script.js
@@ -1,33 +1,10 @@
 const progress = document.querySelector("#progress");
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
-const circles = document.querySelectorAll(".circle");
-let currentActive = 1;
+const circle = document.querySelectorAll(".circle");
+let currentActive=1;
+next.addEventListener("click", () => {   
+  currentActive++
 
-next.addEventListener("click", () => {
-  currentActive++;
-  if (currentActive = 1) {
-   
-    circles[currentActive-1].classList.remove("border-blue-500")
-    circles[currentActive-3].classList.add("border-blue-500")
-
-  }
+  })
   console.log(currentActive)
-});
-circles[currentActive-2].classList.add("border-blue-500")
-// prev.addEventListener("click", () => {
-//   currentActive--;
-//   if (currentActive < 1) {
-//     currentActive = 1;
-//   }
-//   update();
-// });
-// function update(){
-//   circles.forEach((circle,idx)=>{
-//     if(idx< currentActive){
-//       circle.classList.add("border-blue-500")
-//     } else {
-//       circle.classList.remove("border-blue-500")
-//     }
-//   })
-// }
