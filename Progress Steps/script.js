9d30c17156f6e88036b210e7facfc454c79ef3ed
Aliteymourinia <aliteywb@gmail.com>
2023-11-25 20:44:47 +0330

    work on js

--- Progress Steps/script.js
+++ Progress Steps/script.js
@@ -2,21 +2,40 @@ const progress = document.querySelector("#progress");
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
 const circles = document.querySelectorAll(".circle");
-let currentActive = 1;
-const pixelValues = [, 380, 600, 990];
+let currentActive = 0;
+circles.length === currentActive;
+const pixelValues = [, 260, 560, 860];
 next.addEventListener("click", () => {
   currentActive++;
-  progress.style.width = pixelValues[currentActive - 1] + "px";
-  circles[currentActive - 1].style.borderColor = "#3B82F6";
-  prev.style.backgroundColor = "#3B82F6";
-  prev.style.borderColor = "#3B83F6";
+  prev.style.border = "#3B83F6";
+  prev.style.borderStyle = "#3B83F6";
   prev.disabled = false;
-  prev.addEventListener("click", () => {
-    currentActive--;
-    if (currentActive >= 1) {
-      progress.style.width = pixelValues[currentActive - 1] + "px";
+  if (currentActive > circles.length) {
+    currentActive === circles.length;
+  }
+  progress.style.width = pixelValues[currentActive] + "px";
+  circles.forEach((circle, index) => {
+    if (index === currentActive) {
+      circle.style.borderColor = "#3B83F6";
     }
   });
+  console.log(currentActive);
+});
+prev.addEventListener("click", () => {
+  prev.classList.add("active:scale-95");
+  currentActive--;
+  if (currentActive > 0) {
+    progress.style.width = pixelValues[currentActive] + "px";
+  } else {
+    prev.disabled = true;
+    progress.style.width = "";
+    circles.forEach((circle, index) => {
+
+      if (index === currentActive) {
+        circle.style.borderColor = "#3B83F6";
+      }
+    }
+  }
 });
 
 console.log(currentActive);
