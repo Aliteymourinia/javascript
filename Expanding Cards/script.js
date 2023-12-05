4445576ea9902b6ab6fcb283bade30153a3061c7
Aliteymourinia <aliteywb@gmail.com>
2023-12-05 18:57:46 +0330

    change something

--- Expanding Cards/script.js
+++ Expanding Cards/script.js
@@ -1,12 +1,4 @@
-// const panels = document.querySelectorAll(".panel");
-// panels.forEach((panel) => {
-//   panel.addEventListener("click", () => {
-//     panels.forEach((otherPanel) => {
-//       otherPanel.classList.remove("grow");
-//     });
-//     panel.classList.add("grow");
-//   });
-// });
+
 const container = document.querySelector(".container");
 const panels = container.querySelectorAll(".panel");
 
