97b7ea223e975060872c75466d1218371c098425
Aliteymourinia <aliteywb@gmail.com>
2023-12-09 20:39:26 +0330

    practice some function

--- daily practice/script.js
+++ daily practice/script.js
@@ -1,6 +1,12 @@
-function sing(song) {
-  console.log(song);
-}
-sing("laa dee daram daa");
-sing("hellooooooooooo");
-sing("babyyyyyyyyyyyy");
+// function sing(song) {}
+// sing("laa dee daram daa");
+// sing("hellooooooooooo");
+// sing("babyyyyyyyyyyyy");
+
+// function multiply(a, b) {
+//   return a - b;
+// }
+// multiply(4, 10);
+// function show() {
+//   alert("hey its wrong");
+// }
