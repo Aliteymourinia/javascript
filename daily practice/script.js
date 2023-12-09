ecc6c9d6b0f1b633abb67953f94936971cb8a6a6
Aliteymourinia <aliteywb@gmail.com>
2023-12-09 21:01:01 +0330

    practice javascript functions

--- daily practice/script.js
+++ daily practice/script.js
@@ -10,3 +10,16 @@
 // function show() {
 //   alert("hey its wrong");
 // }
+function multiply(a, b) {
+  if (a > 10 || b > 10) {
+    return "that's too hard";
+  } else {
+    return a * b;
+  }
+}
+console.log(multiply(5, 3));
+/* function multiply(a, b) {
+//   return a * b;
+// }
+// alert(multiply(5, 4));
+*/
