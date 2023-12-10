4b0e809dde76db24f8fedabe4ed26b621468709e
Aliteymourinia <aliteywb@gmail.com>
2023-12-10 10:49:17 +0330

    practice array

---
+++ js data structures/script.js
@@ -0,0 +1,11 @@
+var list = ["tiger", "cat", "bear", "bird"];
+var numbers = [1, 2, 3, 4];
+var booleans = [true, false];
+var functionList = [
+  function apple() {
+    console.log(functionList);
+  },
+];
+var mixList = ["apple", 1, true, undefined, function () {}];
+
+// console.log(mixList);
