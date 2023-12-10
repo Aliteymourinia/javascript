15cdeb096a3778d9d555fddd50c4dec7979488f0
Aliteymourinia <aliteywb@gmail.com>
2023-12-10 11:31:57 +0330

    practice array methods

--- js data structures/script.js
+++ js data structures/script.js
@@ -1,11 +1,16 @@
-var list = ["tiger", "cat", "bear", "bird"];
+var list = ["tiger", "cat", "bird"];
+list.pop("bird"); //  array method for remove selected value.
+list.push("dog"); // array method for add value to end of list.
+// list.shift(); array method for remove first one value.
+//list.unshift(); ,,,  ,,,,,, for undo deleted value.
+console.log(list);
 var numbers = [1, 2, 3, 4];
 var booleans = [true, false];
 var functionList = [
   function apple() {
-    console.log(functionList);
+    // console.log(functionList);
   },
 ];
-var mixList = ["apple", 1, true, undefined, function () {}];
 
-// console.log(mixList);
+var colors = [["red", "blue", "green"]]; //we can have array in array.
+var mixList = ["apple", 1, true, undefined, function () {}];
