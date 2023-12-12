0c6ff084f4cd36fce2d56fa142ef695780183457
Aliteymourinia <aliteywb@gmail.com>
2023-12-12 19:23:54 +0330

    create form and database

---
+++ simple facebook sign in/script.js
@@ -0,0 +1,16 @@
+const database = [
+  {
+    username: "ali",
+    password: "pss9932",
+  },
+];
+const newsFeed = [
+  {
+    username: "mohsen",
+    timeline: "so tired from all that learning!",
+  },
+  {
+    username: "sara",
+    timeline: "Javascript is so cool!",
+  },
+];
