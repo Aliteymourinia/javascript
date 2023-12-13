167c62ebc3e4e13a17393fa89b0bc60e747f4d82
Aliteymourinia <aliteywb@gmail.com>
2023-12-13 14:59:33 +0330

    add empty field required error

--- simple facebook sign in/script.js
+++ simple facebook sign in/script.js
@@ -1,25 +1,28 @@
 const submit = document.querySelector("#submit");
 const userName = document.querySelector("#userName");
 const passWord = document.querySelector("#passWord");
-const database = [
+const emptyField = document.querySelector("#emptyField");
+const error = document.querySelector("#error");
+database = [
   {
     username: "ali",
-    password: "pss9932",
+    password: "pss",
   },
 ];
-const newsFeed = [
-  {
-    username: "mohsen",
-    timeline: "so tired from all that learning!",
-  },
-  {
-    username: "sara",
-    timeline: "Javascript is so cool!",
-  },
-];
-submit.addEventListener("click", () => {
-  if (userName === database[0].username && passWord === database[0].password) {
-    prompt("welcome");
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  const userNameValue = userName.value;
+  const passWordValue = passWord.value;
+  if (userName.Value == null && passWord.Value == null) {
+    emptyField.classList.remove("hidden");
+  } else if (
+    userNameValue !== database[0].username ||
+    passWordValue !== database[0].password
+  ) {
+    emptyField.classList.add("hidden");
+    error.classList.remove("hidden");
   } else {
+    error.classList.add("text-green-700");
+    error.innerHTML = "welcome :)";
   }
 });
