1e6f120bd0b29e7771ca5f288920b19dec5f5635
Aliteymourinia <aliteywb@gmail.com>
2023-12-18 19:22:10 +0330

    practice loop (for)

--- Loop/script.js
+++ Loop/script.js
@@ -5,7 +5,7 @@ const todos = [
   "study javascript",
   "go shopping",
 ];
-var todosLength = todos.length; //this define todos=5 it means todosLength = todos.length
-for (var i = 0; i < todosLength; i++) {
-  todos.pop();
-}
+// var todosLength = todos.length;    //this define todos=5 it means todosLength = todos.length
+// for (var i = 0; i < todosLength; i++) {
+//   todos.pop();
+// }
