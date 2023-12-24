d810ad1f960ea4748e7c78a829fc564de756c307
Aliteymourinia <aliteywb@gmail.com>
2023-12-24 19:47:55 +0330

    test

--- test2/script.js
+++ test2/script.js
@@ -1,3 +1,8 @@
 const button = document.querySelector("button");
 const ol = document.querySelector("ol");
 
+button.addEventListener("click", () => {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode("toyota"));
+  ol.appendChild(li);
+});
